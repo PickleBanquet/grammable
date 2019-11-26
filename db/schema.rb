60e7a4a3766cb2f6dee66d78c963d1cd43cdd40c
Michael Dunphy <michaeldunphy52@gmail.com>
2019-11-26 18:32:58 +0000

    carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_014804) do
+ActiveRecord::Schema.define(version: 2019_11_26_182318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
